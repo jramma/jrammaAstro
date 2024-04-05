680d0b1cf66d4f0c47d36118df4a6020871e1465
jramma <pepe.ramos@mortensen.cat>
2024-04-05 17:32:27 +0200

    ✨ foto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon({
+    svgoOptions: {
+      plugins: [
+        "preset-default",
+        {
+          name: "convertColors",
+          params: { currentColor: true },
+        },
+      ],
+    },
+  })]
 });
