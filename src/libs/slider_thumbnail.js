50b5f30c67b8faecac6d7afb75a89df82ef0093b
jramma <pepe.ramos@mortensen.cat>
2024-05-03 15:39:13 +0200

    🎉 swiper astro

--- src/libs/slider_thumbnail.js
+++ src/libs/slider_thumbnail.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, FreeMode, Thumbs, Mousewheel } from 'swiper/modules';
+import { Navigation, FreeMode, Thumbs, Mousewheel, Autoplay} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/free-mode';
@@ -11,22 +11,45 @@ document.addEventListener('DOMContentLoaded', function() {
     const thumbs_swiper = new Swiper(".thumbs", {
       modules:[FreeMode],
       spaceBetween: 3,
-      slidesPerView: 3,
+      slidesPerView: "auto",
       freeMode: true,
       watchSlidesProgress: true,
     });
     const main_swiper = new Swiper(".main", {
-      modules: [Navigation,Thumbs, Mousewheel],
-      watchSlidesProgress: 3,
-      spaceBetween: 3,
-      slidesPerView: 4,
+      modules: [Navigation,Thumbs, Mousewheel,Autoplay],
+      spaceBetween: 4,
+      slidesPerView: 2,
       mousewheel: true,
+      freeMode: true,
+      loop: true,
       navigation: {
-        nextEl: ".next",
-        prevEl: ".prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 1500,
       },
       thumbs: {
         swiper: thumbs_swiper,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 4,
+        },
+        768: {
+          freeMode: false,
+          slidesPerView: 3,
+          spaceBetween: 4,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 4,
+        },
+        1280: {
+          slidesPerView: 7,
+          spaceBetween: 4,
+        },
       },
     });
 });
